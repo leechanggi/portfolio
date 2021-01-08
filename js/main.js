1f249e870f855e686309202f0884ce345e0570b1
leechanggi <lxyex1379@naver.com>
2021-01-08 20:45:16 +0900

    formal
    2021/01/08_20:45

--- js/main.js
+++ js/main.js
@@ -12,3 +12,40 @@ function windeowResizeHandler() {
     }
 }
 $(window).trigger('resize');
+
+function scrollGnbFnc() {
+    $(window).scroll(function () {
+        if ($('html').hasClass('pc')) {
+            var rowHeight = $('.row1-wrap').offset().top + $('.row1-wrap').outerHeight();
+            var scrollTop = $(window).scrollTop()
+            if (scrollTop > rowHeight) {
+                $('#header').addClass('on')
+            }
+            if (scrollTop < rowHeight) {
+                $('#header').removeClass('on')
+            }
+        }
+    });
+}
+
+function allmenuFnc() {
+    var allmenuListBtn = $('.all-menu > ul > li > a')
+    allmenuListBtn.click(function () {
+        if (allmenuListBtn.next().hasClass('active')) {
+            $(this).next().removeClass('active')
+        } else {
+            $(this).next().addClass('active');
+        }
+    })
+    $('.btn-all-menu-open').click(function () {
+        $('.all-menu').css('right','0')
+    })
+    $('.btn-all-menu-close').click(function () {
+        $('.all-menu').css('right','-100vw')
+    })
+}
+
+$(function () {
+    scrollGnbFnc();
+    allmenuFnc();
+});
